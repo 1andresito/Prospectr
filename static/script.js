d42dae6201c96566cfd11e72de56004d54ade509
1andresito <notandresito@gmail.com>
2026-07-30 16:57:53 -0500

    generalize

--- static/script.js
+++ static/script.js
@@ -1,5 +1,5 @@
 // script.js
-const map = L.map("map").setView([43.0731, -89.4012], 13);
+const map = L.map("map").setView([39.8283, -98.5795], 4);
 
 L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png", {
     attribution: "&copy; OpenStreetMap contributors",
